d547dde4f092c8a5ac1f11fe60f0c010c981a554
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-05-28 23:24:46 +0700

    feat: prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+const config = {
+  bracketSpacing: true,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: false,
+  semi: true,
+  printWidth: 80,
+  plugins: [
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+  ],
+  pluginSearchDirs: false,
+};
+
+module.exports = config;
